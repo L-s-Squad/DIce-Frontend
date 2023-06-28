f6be6f3f3b5bd800019650512ef251d1bcf92cc9
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-28 12:00:39 +0530

    dice

---
+++ src/Components/DiceScreen.js
@@ -0,0 +1,34 @@
+
+
+const DiceScreen = () => {
+
+
+
+    return(
+        <div>
+            <label>Person1: 
+                <input type="text" name="person1" />
+                <button>Roll</button>
+             </label>
+            <label>Person2:
+                <input type="text" name="person2" />
+                <button>Roll</button>
+            </label>
+            <label>Person3:
+                <input type="text" name="person3" />
+                <button>Roll</button>
+            </label>
+            <label>Person4:
+                <input type="text" name="person4" />
+                <button>Roll</button>
+            </label>
+            <label>Person5:
+                <input type="text" name="person5" />
+                <button>Roll</button>
+            </label>
+            
+        </div>
+    )
+} 
+
+export default DiceScreen;
