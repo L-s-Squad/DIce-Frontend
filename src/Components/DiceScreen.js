f07992624982e507b654d42009fbb43c74c9a3dd
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-28 13:32:39 +0530

    winner and delete

--- src/Components/DiceScreen.js
+++ src/Components/DiceScreen.js
@@ -9,6 +9,10 @@ const DiceScreen = () => {
         player1: "",player2: "",player3: "",player4: "",player5: ""
     })
 
+    let [winner, setWinner] = useState([]);
+    let [winnerScore, setWinnerScore] = useState("");
+
+    console.log(winner, winnerScore)
 
 
     function diceRoll(e) {
@@ -19,9 +23,34 @@ const DiceScreen = () => {
           axios.post("http://localhost:5000/api/player", {name: players[player_id]})
             .then(res => setScores({...scores, [player_id]: res.data.data.player.score}))
             .catch(err => console.log(err))
-
-
          }
+
+    function findWinner() {
+          
+        axios.get("http://localhost:5000/api/winner")
+            .then(res => {
+                setWinner(res.data.data.winnerArray);
+                setWinnerScore(res.data.data.highestScore);
+            })
+            .catch(err => console.log(err))
+
+    }
+
+    function reset() {
+          axios.delete("http://localhost:5000/api/reset")
+          .then(res => {
+                setPlayers({
+                    player1: '',player2: '',player3: '',player4: '',player5: ''
+                });
+                setScores({
+                    player1: "",player2: "",player3: "",player4: "",player5: ""
+                });
+                setWinner([]);
+                setWinnerScore("");
+          }
+          )
+            .catch(err => console.log(err))
+    }
 
 
     return(
@@ -29,6 +58,7 @@ const DiceScreen = () => {
             <label>Person1: 
                 <input type="text" name="person1" 
                    onChange={(e) => setPlayers({...players, player1: e.target.value})}
+                    value={players.player1}
                 />
                 <button id="p1" onClick={diceRoll}>Roll</button>
                 {
@@ -38,6 +68,7 @@ const DiceScreen = () => {
             <label>Person2:
                 <input type="text" name="person2" 
                      onChange={(e) => setPlayers({...players, player2: e.target.value})}
+                     value={players.player2}
                 />
                 <button id="p2" onClick={diceRoll}>Roll</button>
                 {
@@ -47,6 +78,7 @@ const DiceScreen = () => {
             <label>Person3:
                 <input type="text" name="person3" 
                         onChange={(e) => setPlayers({...players, player3: e.target.value})}
+                        value={players.player3}
                 />
                 <button id="p3" onClick={diceRoll}>Roll</button>
                 {
@@ -56,6 +88,7 @@ const DiceScreen = () => {
             <label>Person4:
                 <input type="text" name="person4" 
                         onChange={(e) => setPlayers({...players, player4: e.target.value})}
+                        value={players.player4}
                 />
                 <button id="p4" onClick={diceRoll}>Roll</button>
                 {
@@ -65,6 +98,7 @@ const DiceScreen = () => {
             <label>Person5:
                 <input type="text" name="person5" 
                         onChange={(e) => setPlayers({...players, player5: e.target.value})}
+                        value={players.player5}
                 />
                 <button id="p5" onClick={diceRoll}>Roll</button>
                 {
@@ -72,8 +106,18 @@ const DiceScreen = () => {
                 }
             </label>
 
-            <button> End Game </button>
+            <button onClick={findWinner}> End Game </button>
+
+            {
+                winnerScore && (
+                    <div> 
+                        <h1>Winner: {winner.join(",")}</h1>
+                        <h1>Score: {winnerScore}</h1>
+                    </div>
+                )
+            }
             
+            <button onClick={reset}>Reset</button>
         </div>
     )
 } 
